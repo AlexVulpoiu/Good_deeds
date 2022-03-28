8215a587cfc543e56b8e1b064a2ae229d0ceabde
AlexVulpoiu <vulpoiu_alexandru@yahoo.com>
2022-03-28 16:28:58 +0300

    initial commit

---
+++ src/main/java/com/softbinator_labs/project/good_deeds/GoodDeedsApplication.java
@@ -0,0 +1,13 @@
+package com.softbinator_labs.project.good_deeds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodDeedsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodDeedsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softbinator_labs/project/good_deeds/GoodDeedsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softbinator_labs.project.good_deeds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodDeedsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
